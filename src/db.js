1fa45adfc3e21affc4f14678c32d3851816eb253
marsavil <msvillaescusa@gmail.com>
2023-02-07 13:11:21 -0300

    server adjustment

--- src/db.js
+++ src/db.js
@@ -1,17 +1,27 @@
 require ('dotenv').config()
 const {Sequelize} =require ('sequelize')
 
+
 // Credenciales //
 const {
     DB_USER,
     DB_PASS,
     DB_HOST,
-    DB_NAME
+    DB_NAME,
+    DB_DEPLOY
 } = process.env
 
 //Conexion  a la Base de Datos
 
-const DB = new Sequelize(`postgres://${DB_USER}:${DB_PASS}@${DB_HOST}/${DB_NAME}`,{
+// const DB = new Sequelize(`postgres://${DB_USER}:${DB_PASS}@${DB_HOST}/${DB_NAME}`,{
+//     logging: false,
+//     native: false,
+//     define: {
+//     timestamps: false
+    
+// }})
+
+const DB = new Sequelize(DB_DEPLOY,{
     logging: false,
     native: false,
     define: {
