c9abbfac0f477cd244b35474798f4013bcc2ea0f
Mariano Villaescusa <84275466+marsavil@users.noreply.github.com>
2023-02-21 20:58:14 -0300

    Update product.js

--- src/routes/product.js
+++ src/routes/product.js
@@ -1,6 +1,5 @@
 let express = require("express");
 let router = express.Router() ;
-//let simpleResponse =require('../middleware/simpleresponse');
 const postProduct = require('../Controllers/postProduct.js');
 const getProductDB = require("../Controllers/DBgetProducts.js");
 const getProductById = require("../Controllers/getProductById");
@@ -16,7 +15,6 @@ router.get('/:id', getProductById)
 router.post('/add', postProduct)
 router.put('/hide', hideProduct)
 router.put('/restore', restoreProduct)
-//router.patch('/',simpleResponse)
 router.get('/orderName', orderProductByBrand )
 router.get('/orderPrice', orderByPrice )
 
