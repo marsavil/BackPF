1cbe3d7c9af15a16e1b74b51962b73ed7b9133d6
Mariano Villaescusa <msvillaescusa@gmail.com>
2023-02-21 22:41:37 -0300

    actualizacion 21/02

--- src/app.js
+++ src/app.js
@@ -35,7 +35,7 @@ app.use(express.json());
 
 //deploy
 app.use((req,res,next)=>{
-  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Origin', 'https://frontpf-production-eafa.up.railway.app/')
   res.header('Access-Control-Allow-Credentials', 'true');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
