6d1ce9194e5cc3d0c69ed0e219662517010c920d
Mariano Villaescusa <msvillaescusa@gmail.com>
2023-02-21 21:23:48 -0300

    actualizacion 21/02

--- src/db.js
+++ src/db.js
@@ -13,22 +13,22 @@ const {
 //Conexion  a la Base de Datos
 
 //Local
-const DB = new Sequelize(`postgres://${DB_USER}:${DB_PASS}@${DB_HOST}/${DB_NAME}`,{
+// const DB = new Sequelize(`postgres://${DB_USER}:${DB_PASS}@${DB_HOST}/${DB_NAME}`,{
+//     logging: false,
+//     native: false,
+//     define: {
+//     timestamps: false
+    
+}})
+
+//Deploy
+const DB = new Sequelize(DB_DEPLOY,{
     logging: false,
     native: false,
     define: {
     timestamps: false
     
 }})
-
-//Deploy
-// const DB = new Sequelize(DB_DEPLOY,{
-//     logging: false,
-//     native: false,
-//     define: {
-//     timestamps: false
-    
-// }})
 
 module.exports = DB
 
