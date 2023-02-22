32bbf147ba5eeb799db136ea8c0c33848c74c129
Mariano Villaescusa <msvillaescusa@gmail.com>
2023-02-21 23:12:49 -0300

    actualizacion 21/03

--- src/Controllers/getUser.js
+++ src/Controllers/getUser.js
@@ -1,19 +1,16 @@
+const User = require("../models/User.js");
 
-const  User = require('../models/user.js');
-
-
-const getUser = async(req, res)=>{
-    const id = req.params.id;
-    try {
-        const search = await User.findOne({
-            where: {
-                id: id
-            }
-        })
-        res.status(200).send(search)
-
-    } catch (error) {
-        res.status(400).send(error)
-    } 
-}
+const getUser = async (req, res) => {
+  const id = req.params.id;
+  try {
+    const search = await User.findOne({
+      where: {
+        id: id,
+      },
+    });
+    res.status(200).send(search);
+  } catch (error) {
+    res.status(400).send(error);
+  }
+};
 module.exports = getUser;
